f3abdb9bf16e9fa6e9d4c6686dda289c19275980
pc <pc@DESKTOP-A9S084Q>
2019-02-15 15:07:37 +0900

    initial commit

---
+++ src/Project/Test.java
@@ -0,0 +1,16 @@
+package Project;
+
+import java.util.Scanner;
+
+class Test{
+	public static void main(String[] args) {
+		final double PI=3.1416;
+		Scanner stdIn=new Scanner(System.in);
+		
+		System.out.print("반지름: ");
+		double r=stdIn.nextDouble();
+		
+		System.out.println("원주의 길이는"+2*PI*r+"입니다.");
+		System.out.println("면적은"+PI*r*r+"입니다.");
+	}
+}
